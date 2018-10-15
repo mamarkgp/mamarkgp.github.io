9ee0afa5b4da40236324c68da85be985470f0db7
jm.deguinion <jm.deguinion@172-15-14-132.lightspeed.sntcca.sbcglobal.net>
2018-10-15 14:39:49 +0800

    handle element null error

--- wellnessfeaturedcontent/js/eventListener.js
+++ wellnessfeaturedcontent/js/eventListener.js
@@ -1,9 +1,11 @@
 // addEventListener support for IE8
 function bindEvent(element, eventName, eventHandler) {
-    if (element.addEventListener) {
-        element.addEventListener(eventName, eventHandler, false);
-    } else if (element.attachEvent) {
-        element.attachEvent('on' + eventName, eventHandler);
+    if (element != null || element != undefined) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, eventHandler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + eventName, eventHandler);
+        }
     }
 }
 // Send a message to the parent
